9c3ce8fd1061c2a20e7f055a59745e91f175dc55
Illya Sigov <isigov@bu.edu>
2018-01-29 17:12:06 -0500

    Added image counter

--- sys.py
+++ sys.py
@@ -3,32 +3,32 @@ import twittermedia
 import urllib2
 import os
 
-class 
 
 def main():
 	try:
 		tw = twittermedia.TwitterMedia()
-		urls = tw.FetchImages("isigov95")
+		urls = tw.FetchImages("barstoolsports", 100)
+		print len(urls)
 	except:
-		print("Error connecting to Twitter\n")
+		raise ValueError("Error connecting to Twitter")
 		return
 
-	counter = 0
-	for url in urls:
-		datatowrite = ""
-		try:
-			filedata = urllib2.urlopen(url)  
-			datatowrite = filedata.read()
-		except:
-			print("Error downloading image from {0}\n", url)
-			continue
+	# counter = 0
+	# for url in urls:
+	# 	datatowrite = ""
+	# 	try:
+	# 		filedata = urllib2.urlopen(url)  
+	# 		datatowrite = filedata.read()
+	# 	except:
+	# 		raise ValueError("Error downloading image from {0}\n", url)
+	# 		continue
 
-		with open('images/image-%04d.jpg' % counter, 'wb') as f:  
-			f.write(datatowrite)
+	# 	with open('images/image-%04d.jpg' % counter, 'wb') as f:  
+	# 		f.write(datatowrite)
 
-		counter += 1
-	ff = ffmpeg.ffmpeg()
-	ff.img2vid(os.getcwd() + "/images", "mov.mp4")
+	# 	counter += 1
+	# ff = ffmpeg.ffmpeg()
+	# ff.img2vid(os.getcwd() + "/images", "mov.mp4")
 
 
 if __name__ == '__main__':
--- twittermedia.py
+++ twittermedia.py
@@ -10,12 +10,12 @@ class TwitterMedia:
 		access_token_key='2492938182-FC5nBZKdfor74sqdzA4qvqCgH9BQusYO7wy14pX',
 		access_token_secret='ync9VcW2xj23HStbntPI3A3S9MXG9SbtkblVTDcUo6arb')
 
-	def FetchImages(self, feed):
+	def FetchImages(self, feed, amt=-1):
 		statuses = self.api.GetUserTimeline(screen_name=feed, count=200)
 		last_id = statuses[-1].id-1
 
 		while True:
-			more_statuses = self.api.GetUserTimeline(screen_name="isigov95", count=200, max_id=last_id)
+			more_statuses = self.api.GetUserTimeline(screen_name=feed, count=200, max_id=last_id)
 			if len(more_statuses) < 200:
 				statuses = statuses + more_statuses
 				break
@@ -28,5 +28,8 @@ class TwitterMedia:
 			if not s.media == None:
 				if s.media[0].type == "photo":
 					images.append(s.media[0].media_url)
-
+					if amt > 0:
+						amt -= 1
+						if amt == 0:
+							break
 		return images
