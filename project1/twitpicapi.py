c441fb088a15d1916c94bb9f1d458257c3af733b
Illya Sigov <illyasigov@dhcp-wifi-8021x-168-122-12-122.bu.edu>
2018-04-12 11:39:54 -0400

    Added 3 days of trending terms

--- project1/twitpicapi.py
+++ project1/twitpicapi.py
@@ -17,19 +17,23 @@ class TwitpicAPI:
 	def Aggregate(self, feed, count, download_location, video_location, ffmpeg_location):
 		url_list = self.TwitterGetList(feed, count)
 		self.TwitterDownload(url_list, download_location)
-		self.TwitterImage2Video(download_location, video_location, ffmpeg_location)
+		#self.TwitterImage2Video(download_location, video_location, ffmpeg_location)
 		return self.TwitterAnalyzeImage(download_location, feed)
 
 	def SaveMongo(self, data, database, collection):
 		mongo_db = self.client[database]
 		mongo_co = mongo_db[collection]
 		result = mongo_co.insert_one(data)
-		#print('One post: {0}'.format(result.inserted_id))
+
+	def outputMongo(self):
+		for post in self.client['tweets']['savedTerms'].find().sort('count', -1):
+			print str(post['name']) + " " + str(post['count'])
 
 	def TwitterGetList(self, feed, count):
 		try:
 			tw = twittermedia.TwitterMedia(self.lconsumer_key, self.lconsumer_secret, self.laccess_token_key, self.laccess_token_secret)
 			urls = tw.FetchImages(feed, count)
+			print "Fetched {0} images from {1}...".format(feed, count)
 			return urls
 		except:
 			raise ValueError("Error connecting to Twitter")
@@ -52,7 +56,7 @@ class TwitpicAPI:
 			except:
 				raise ValueError("Error saving images")
 				return
-
+			print "Downloaded from {0} & saved to {1}".format(url, os.path.join(location, 'image-%04d.jpg' % counter))
 			counter += 1
 
 	def TwitterImage2Video(self, image_folder, save_file, ffmpeg_location, fps=1):
@@ -61,32 +65,38 @@ class TwitpicAPI:
 		try:
 			if os.path.getsize(save_file) == 0:
 				raise ValueError("Error converting images to video")
+			print "Converted images from {0} to video file {1}".format(image_folder, save_file)
 		except:
 			raise ValueError("Error converting images to video")
 
 	def TwitterAnalyzeImage(self, image_folder, feed):
 		gg = vision_api.GoogleVisionWrapper(self.ljson_key)
 		labels = []
-		#try:
-		for img_name in os.listdir(image_folder):
-			if img_name.endswith(".jpg"):
-				out_labels = gg.AnalyzeImage(os.path.join(image_folder, img_name))
-				for s in out_labels:
-					post = self.client['tweets']['savedTerms'].find_one({'name': s})
-					if post is not None:
-						self.client['tweets']['savedTerms'].update_one({'name': s}, {"$set": {'name': s, 'count': int(post['count']) + 1}})
-					proto = {
-						'name': s,
-						'count' : 1
+		try:
+			for img_name in os.listdir(image_folder):
+				if img_name.endswith(".jpg"):
+					print "Analyzing {0}...".format(img_name)
+					out_labels = gg.AnalyzeImage(os.path.join(image_folder, img_name))
+					for s in out_labels:
+
+						post = self.client['tweets']['savedTerms'].find_one({'name': s})
+						if post is not None:
+							print "{0} exists, adding to count".format(s)
+							self.client['tweets']['savedTerms'].update_one({'name': s}, {"$set": {'name': s, 'count': int(post['count']) + 1}})
+						proto = {
+							'name': s,
+							'count' : 1
+						}
+						print "{0} doesn't exist, creating new".format(s)
+						self.SaveMongo(proto, 'tweets', 'savedTerms')
+					labels.append(out_labels)
+					post_1 = {
+						'handle': feed,
+						'img_name': img_name,
+						'labels': ','.join(out_labels)
 					}
-					self.SaveMongo(proto, 'tweets', 'savedTerms')
-				labels.append(out_labels)
-				post_1 = {
-					'handle': feed,
-					'img_name': img_name,
-					'labels': ','.join(out_labels)
-				}
-				self.SaveMongo(post_1, "tweets", "images")
-		return labels
-		#except:
-		#	raise ValueError("Error accessing image directory")
+					print "Saved image {0} to MongoDB".format(img_name)
+					self.SaveMongo(post_1, "tweets", "images")
+			return labels
+		except:
+			raise ValueError("Error accessing image directory")
